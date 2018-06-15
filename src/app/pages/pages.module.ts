1d229ec407304a3463769acece98c035e3699f8d
nicolas <nicolasbloj@gmail.com>
2018-06-15 12:28:28 -0300

    incrementador en progressbar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { PagesComponent } from './pages.component';
 
@@ -12,6 +13,8 @@ import { PAGES_ROUTES } from './pages.routes';
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
     SharedModule,
     RouterModule,
     PAGES_ROUTES
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso = 50;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  cambiarValor(valor: number): void {
+
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    } else if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+
+    this.progreso = this.progreso + valor;
+  }
 }
