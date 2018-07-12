8355c965b1bad83b519ec1c905cff258c59961ff
nicolas <nicolasbloj@gmail.com>
2018-07-12 16:49:46 -0300

    sidebar MENU

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: any = [
+    {
+      titulo: 'Principal',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Dashboard', url: '/dashboard' },
+        { titulo: 'ProgressBar', url: '/progress' },
+        { titulo: 'Gráficas', url: '/charts1' }
+      ]
+    }
+  ];
   constructor() { }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { NopagefoundComponent } from './404/nopagefound.component';
 import { BreadcumbsComponent } from './breadcumbs/breadcumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarService } from '../services/service.index';
+
+
 
 @NgModule({
+    imports :[
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         NopagefoundComponent,
         BreadcumbsComponent,
@@ -17,7 +26,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
         BreadcumbsComponent,
         HeaderComponent,
         SidebarComponent
-    ]
+    ],
+    providers:[SidebarService]
 })
 export class SharedModule {}
 
